bc3d746158a3845da468fb04d01f880c5dca58b6
Alexander-Stanimirov <48020686+Alexander-Stanimirov@users.noreply.github.com>
2020-12-03 20:01:25 +0100

    Update 02_p2.py

--- 02_p2.py
+++ 02_p2.py
@@ -1,12 +1,11 @@
-f = open("C:/Users/Bearson/Documents/2020_aoc/02.txt", "r")
+f = open("C:/Users/Username/Documents/2020_aoc/02.txt", "r")
 l = f.read().split("\n")
 
 data = []
 for line in l:
     data.append(line.replace(":", "").replace("-", " ").split(" "))
 
-valid = 0
-flag = 0
+valid = flag = 0
 
 for rule in data:
     for i in range(len(rule[3])):
